7467a293bf29cff4c2df6b72e27702d934c8b4d8
Ankur Chauhan <ankurtrojan@gmail.com>
2014-11-21 00:08:41 -0800

    Use tokumx

--- bin/stop-cluster.sh
+++ bin/stop-cluster.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if sudo docker ps | grep "wdalmut/mongodb" >/dev/null; then
-    sudo docker ps | grep "wdalmut/mongodb" | awk '{ print $1 }' | xargs -r sudo docker stop >/dev/null
+if sudo docker ps | grep "ankurcha/tokumx" >/dev/null; then
+    sudo docker ps | grep "ankurcha/tokumx" | awk '{ print $1 }' | xargs -r sudo docker stop >/dev/null
     echo "Stopped the cluster and cleared all of the running containers."
 fi
